788a2ebc413c9c6ce135a1ea70fff26794b8e7a0
ludmiladichiera <ludmiladichiera@gmail.com>
2025-05-04 14:53:28 -0300

    arquitectura en capas, agregue dominio y negocio del proyecto anterior, actualice el nombre de la base de datos

---
+++ Dominio/Categoria.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+
+        public Categoria() { }
+        public Categoria(int id, string descripcion)
+        {
+            Id = id;
+            Descripcion = descripcion;
+        }
+
+        public override string ToString()
+        {
+            return Descripcion;
+        }
+
+    }
+}
---
+++ Dominio/Imagen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Imagen
+    {
+
+        public int Id { get; set; }
+        public int IdArticulo { get; set; }
+        public string ImagenUrl { get; set; }
+        public Imagen() { }
+        public Imagen(string img)
+        {
+            ImagenUrl = img;
+        }
+
+        public override string ToString()
+        {
+            return ImagenUrl;
+        }
+
+        public static explicit operator Imagen(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
