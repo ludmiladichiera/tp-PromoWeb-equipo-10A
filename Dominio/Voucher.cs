fb4d96c773c213829d6e709ec7e10cbae137c9da
ludmiladichiera <ludmiladichiera@gmail.com>
2025-05-04 15:27:41 -0300

    clase cliente, voucher en dominio, segun bd

---
+++ Dominio/Voucher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Voucher
+    {
+        public string Codigo { get; set; }
+        public int IdCliente { get; set; }
+        public DateTime FechaCanje { get; set; } //chequear el tipo datetime
+        public int IdArticulo { get; set; }
+
+        //esto nose, me imagino que va a ser necesario
+        public bool Usado { get; set; }
+        public bool EsValido()
+        {
+            return !Usado;
+        }
+
+    }
+}
